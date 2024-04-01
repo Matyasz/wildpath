14bde318a88d290141bcade410ead8cc0aa08840
Taylor Matyasz <tjmatyasz@gmail.com>
2024-03-31 23:14:06 -0700

    feat: added WASM component workspace member

---
+++ wildpath-component/src/main.rs
@@ -0,0 +1,13 @@
+#[allow(warnings)]
+mod bindings;
+
+use wildpath::resolve;
+
+fn main() {
+    let path: &String = &std::env::args().nth(1).expect("Invalid argument for path");
+    let res = resolve(&std::path::Path::new(path)).expect("Failure to resolve path");
+
+    for p in res {
+        println!("{}", p.into_os_string().into_string().unwrap());
+    }
+}
